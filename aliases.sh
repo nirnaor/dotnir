862bbae88cc4e05d989f79f30d053a0c53e8e1c3
nirnaor <nirnaori@gmail.com>
2016-10-30 10:19:01 +0200

    Renamed file.

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+echo "find me at dotnir .bashrc"
+# app
+alias p=python
+alias g=git
+alias gs=git stauts
+alias gb=git branch
+alias gco=git checkout
+alias tm=tmux
+
+
+# private
+alias dotnir=$DOTNIR
+
+
+# Shell stuuf
+alias c=clear
+alias x=exit
