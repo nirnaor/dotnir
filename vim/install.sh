1a534f3af11026d1a736e116121ea6685a004cde
Nir Naor <nirnaori@gmail.com>
2017-03-19 11:05:09 +0200

    Moved to plug and added ag and fzf.

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,5 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 brew install ack
 brew install ctags
 brew install tig
