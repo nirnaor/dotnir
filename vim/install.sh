bd351dab7b33fc30ccfacb47b6deb7411787e39b
Nir Naor <nirnaori@gmail.com>
2017-03-30 17:00:12 +0300

    Updated dotfiles.

--- vim/install.sh
+++ vim/install.sh
@@ -8,7 +8,7 @@ brew install the_silver_searcher
 
 ln -s $DOTNIR/vim/vimrc ~/.vimrc
 ln -s $DOTNIR/vim/tigrc ~/.tigrc
-ln -s $DOTNIR/vim/ackrc ~/.ackrc
+ln -s $DOTNIR/vim/agignore ~/.agignore
 ln -s $DOTNIR/vim/agignore ~/.agignore
 ln -s $DOTNIR/vim/ctags ~/.ctags
 vim +PluginInstall +qall
