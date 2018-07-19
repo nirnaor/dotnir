ad7895ab75680fd868a103724ccb97532c96054c
Nir Naor <nirna@tal-bs-mbp13.local>
2018-07-19 15:37:33 +0300

    wip

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # git clone git@github.com:olivierverdier/zsh-git-prompt.git
-echo DOTNIR_LOCATION=`pwd` >> ~/.zshrc
-ln -s $DOTNIR_LOCATION/zshrc 
+
+dir=`pwd`
+parentdir="$(dirname "$dir")"
+echo $parentdir
+
+# echo DOTNIR_LOCATION=`pwd` >> ~/.zshrc
+echo 'DOTNIR_LOCATION='$parentdir >> ~/.zshrc
+# ln -s $DOTNIR_LOCATION/zshrc 
